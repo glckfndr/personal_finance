571a7b6e8af1e4a6a378e48ceadf2e293aad3ac7
glckfndr <obulan65@gmail.com>
2023-03-05 23:38:38 +0200

    Finish search and work with database

---
+++ test/helpers/partial_helpers_test_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+
+class Partial_helpers_testTest < Minitest::Test
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test_create_table_header
+    res = OperationsHelper.create_table_header(%w[a b c\ d])
+    assert_equal("<tr><th><strong>a</strong></th><th><strong>b</strong></th><th><strong>c d</strong></th><tr>", res,
+    "Method create_table_header is failed!")
+  end
+end
